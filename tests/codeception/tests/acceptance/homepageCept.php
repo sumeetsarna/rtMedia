5b469989caa068843f2f4ad9459c8c46a7b7003d
sumeet <sumeet.sarna@rtcamp.com>
2016-10-13 15:06:53 +0530

    Updated homepageCept file to test with travis

--- tests/codeception/tests/acceptance/homepageCept.php
+++ tests/codeception/tests/acceptance/homepageCept.php
@@ -2,16 +2,13 @@
 // we instantiate acceptance tester class and use I variable
 $I = new AcceptanceTester($scenario);
 $I->wantTo('verify that homepage has text lorem');
-// Need to decalre what page are we on
-// am on root index.php
 $I->amOnPage('/');
-// What i expect to see
 $I->see('lorem');
 //$I->see('lorem', 'h1');  to see lorem within an h1 tag. Heading says lorem
 $I->wantTo('verify that product apple exists');
 $I->click('Apple');
 $I->wantTo('verify that i have reached apple product page');
-$I->seeCurrentUrlEquals('/sumeet/product/apple/') ;
+$I->seeCurrentUrlEquals('/product/apple/') ;
 date_default_timezone_set('America/New_York'); // to avoid timezone error (http://stackoverflow.com/questions/8811603/how-to-fix-php-errors-related-to-timezone-function-strtotime-and-function-date)
 //$I->wantTo('want to add one qty to cart');
 $I->click('.single_add_to_cart_button');
@@ -19,10 +16,9 @@ $I->see('View Cart');
 //$I->click('View Cart');
 $I->click('.woocommerce-message a.button.wc-forward');
 $I->wantTo('verify that am on cart page');
-$I->seeCurrentUrlEquals('/sumeet/cart/') ;
+$I->seeCurrentUrlEquals('/cart/') ;
 $I->click('Proceed to Checkout');
-//$I->seeInCurrentUrl('')
-$I->seeCurrentUrlEquals('/sumeet/checkout/') ;
+$I->seeCurrentUrlEquals('/checkout/') ;
 $I->expect('the form is submitted correctly');
 $I->fillField('billing_first_name', 'Sumeet');
 $I->fillField('billing_last_name','1234');
